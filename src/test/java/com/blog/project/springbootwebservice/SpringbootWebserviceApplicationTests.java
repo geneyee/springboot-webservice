6bab1ffb1ef6816a4e7e22db3c65dc4fdfdf4481
geneyee <geneyeey@gmail.com>
2022-04-09 14:17:32 +0900

    first commit

---
+++ src/test/java/com/blog/project/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.project.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
