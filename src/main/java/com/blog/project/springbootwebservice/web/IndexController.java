076c0baa1cc31fbd51d60a13a5d0da15005583d0
geneyee <geneyeey@gmail.com>
2023-05-08 17:52:09 +0900

    IndexController.java SessionUser @LoginUser

--- src/main/java/com/blog/project/springbootwebservice/web/IndexController.java
+++ src/main/java/com/blog/project/springbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.blog.project.springbootwebservice.web;
 
+import com.blog.project.springbootwebservice.config.auth.LoginUser;
 import com.blog.project.springbootwebservice.config.auth.dto.SessionUser;
 import com.blog.project.springbootwebservice.service.posts.PostsService;
 import com.blog.project.springbootwebservice.web.dto.PostsResponseDto;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -19,10 +21,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
