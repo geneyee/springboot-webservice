0fdebf8480dfe6e21a40d09f7bd85ded01808f21
geneyee <geneyeey@gmail.com>
2023-05-09 12:17:19 +0900

    java11 version 변경

--- src/main/java/com/blog/project/springbootwebservice/config/auth/SecurityConfig.java
+++ src/main/java/com/blog/project/springbootwebservice/config/auth/SecurityConfig.java
@@ -2,33 +2,34 @@ package com.blog.project.springbootwebservice.config.auth;
 
 import com.blog.project.springbootwebservice.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.xml.ws.WebServiceException;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    protected void configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain newFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                        .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .userInfoEndpoint()
+                        .userService(customOAuth2UserService));
+
+        return http.build();
     }
 }
